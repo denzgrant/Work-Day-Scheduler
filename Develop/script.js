cbdd50782282fcc3d4c3f5271d78c6bd7de9f4b6
Denzell Grant <denzgrant@gmail.com>
2020-04-03 21:01:09 -0700

    Tweaked user imput and storing user input. last push until final

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,65 @@
 var timeDiv;
 var inputDiv;
 var saveBtnDiv;
-
-var a = moment().format('MMM Do YYYY, h:mm:ss a');
+var userInput;
+//Displaying Current Date and Time 
+const a = moment().format('MMM Do YYYY, h:mm:a');
 document.getElementById("currentDay").innerHTML = a;
 
+
+const currentHour = moment().format('H');
+console.log(a);
+console.log(currentHour);
 //Time Slots
-var timeSlots = ["8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM", "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM",];
+const timeSlots = ["8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM",];
+const workHours = [20, 20, 10, 11, 12, 13, 14, 15, 16, 17];
 
 
 //Timme Blocks 
 
-for (var i = 0; i < timeSlots.length; i++) {
-    var timeDiv = $("<p>");
-    timeDiv.addClass("hour row description");
+for (i = 0; i < timeSlots.length; i++) {
+    let timeDiv = $("<p>");
+    timeDiv.addClass("hour row");
     timeDiv.text(timeSlots[i]);
     $(".float-left").append(timeDiv);
 }
 
-for (var i = 0; i < timeSlots.length; i++) {
-    var inputDiv = $("<input style='width: 620px;' type='text'placeholder=''/>");
-    inputDiv.addClass("textarea description row");
+for (i = 0; i < timeSlots.length; i++) {
+    let inputDiv = `<input type='text' data-hour='${workHours[i]}' class='textarea row' size='90'placeholder=''/>`;
     $(".float-none").append(inputDiv);
 }
 
-for (var i = 0; i < timeSlots.length; i++) {
-    var saveBtnDiv = $("<p>");
-    saveBtnDiv.addClass("saveBtn saveBtn i:hover");
-    saveBtnDiv.text("save");
+for (i = 0; i < timeSlots.length; i++) {
+    let saveBtnDiv = $("<button class='saveBtn saveBtn:hover'>");
+    saveBtnDiv.text("Save");
     $(".float-right").append(saveBtnDiv);
+
 }
+
+$("button").click(function () {
+    let userInput = $(".textarea").val();
+    localStorage.setItem("User's events", userInput);
+    console.log(userInput);
+});
+
+function retrieveInput() {
+    let userEvents = localStorage.getItem("User's events");
+    console.log(userEvents);
+    $(".textarea:text").val(userEvents)
+    
+}
+
+//add a timer 
+// setInterval(function () {
+$("#shift-hours").each(function () {
+    let hour = $(".textarea.row").attr('data-hour')
+    console.log(hour);
+    console.log(currentHour);
+    if ("data-hour" == currentHour) {
+    $(".textarea.row").css('background', '#ccffee');
+    }
+
+});
+// }, 5000)
+
+retrieveInput(); 
